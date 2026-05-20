f42b30c54960f47392d57ebf80e7e9671ac407a1
TheUnrealLPJ <lucapascal2402@gmail.com>
2026-05-20 23:07:09 +0200

    Updated ssh key

--- users/lpj/nixos.nix
+++ users/lpj/nixos.nix
@@ -11,7 +11,7 @@
     shell = pkgs.bash;
     hashedPassword = "$y$j9T$U.OsKHKNSQc17zW6flJCl1$XbqUsDbLm.TgDOap7YYsVuSw5Sge9R.khYEzhyOtd17";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII1Yr8oOlyzPzumtC9nCm2Gdoz3U+YcRep1FDv3nSzEd testuser@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4VkrL2AxOhWqGJ33piWD7dsT2svlAMhvHpP67wKLg7 lucapascal2402@gmail.com"
     ];
   };
 }
