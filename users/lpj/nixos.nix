41109e4af455a5a9e885a00ea9ebca893258af57
TheUnrealLPJ <lucapascal2402@gmail.com>
2026-05-26 22:26:12 +0200

    Updated user config

--- users/lpj/nixos.nix
+++ users/lpj/nixos.nix
@@ -2,7 +2,7 @@
 
 {
   users.users.root = {
-    hashedPassword = "$y$j9T$U.OsKHKNSQc17zW6flJCl1$XbqUsDbLm.TgDOap7YYsVuSw5Sge9R.khYEzhyOtd17";
+    hashedPassword = "$y$j9T$hMhFOQm8iVBQLX1wOBr6i1$zxLvUO3rZFqzw9reEOmgPOa8niVKZMVmRvUDzjYtFRD";
   };
 
   users.users.lpj = {
@@ -13,7 +13,7 @@
       "wheel"
     ];
     shell = pkgs.bash;
-    hashedPassword = "$y$j9T$U.OsKHKNSQc17zW6flJCl1$XbqUsDbLm.TgDOap7YYsVuSw5Sge9R.khYEzhyOtd17";
+    hashedPassword = "$y$j9T$hMhFOQm8iVBQLX1wOBr6i1$zxLvUO3rZFqzw9reEOmgPOa8niVKZMVmRvUDzjYtFRD";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4VkrL2AxOhWqGJ33piWD7dsT2svlAMhvHpP67wKLg7 lucapascal2402@gmail.com"
     ];
